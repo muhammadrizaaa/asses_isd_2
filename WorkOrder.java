cf5f85a32f77bc4e9154d760f19c59d46ecbada2
muhammadrizaaa <76427345+muhammadrizaaa@users.noreply.github.com>
2024-06-06 09:41:23 +0700

    Add files via upload

--- WorkOrder.java
+++ WorkOrder.java
@@ -1,18 +1,32 @@
 public class WorkOrder implements Comparable {
     private int priority;
-    private String description;
+    private String name;
+    private int umur;
     /**
     * Constructs a work order with a given priority and description.
     *
     * @param aPriority the priority of this work order
-    * @param aDescription the description of this work order
+    * @param aName the description of this work order
+    @param aUmur
     */
-    public WorkOrder(int aPriority, String aDescription) {
-    priority = aPriority;
-    description = aDescription;
+    public WorkOrder(String aPriority, String aName, int aUmur) {
+    if(aPriority.equals("hijau")){
+        priority = 0;
+    }
+    else if(aPriority.equals("merah")){
+        priority = 1;
+    }
+    else if(aPriority.equals("kuning")){
+        priority = 2;
+    }
+    else if(aPriority.equals("biru")){
+        priority = 3;
+    }
+    name = aName;
+    umur = aUmur;
     }
     public String toString() {
-    return "priority=" + priority + ", description=" + description;
+    return "name pasien= " + name + ", usia= " + umur;
     }
     public int compareTo(Object otherObject) {
     WorkOrder other = (WorkOrder) otherObject;
--- main.java
+++ main.java
@@ -1,11 +1,12 @@
 public class main {
     public static void main(String[] args) {
     minHeap q = new minHeap();
-    q.add(new WorkOrder(0, "tes 1"));
-    q.add(new WorkOrder(1, "tes 2"));
-    q.add(new WorkOrder(2, "tes 3"));
-    while (q.size() > 0)
-    System.out.println(q.remove());
+    q.add(new WorkOrder("biru", "Nur Aisyah", 30));
+    q.add(new WorkOrder("merah", "Rima", 20));
+    q.add(new WorkOrder("hijau", "Kira", 10));
+    while (q.size() > 0){
+        System.out.println(q.remove());
+    }
     }
    }
    
